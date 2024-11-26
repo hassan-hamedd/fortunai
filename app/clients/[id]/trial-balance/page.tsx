f224d5dd5e91965d664052388b5f6d2c596bcc13
Hassan Hamed <hassanhamedd1234@gmail.com>
2024-11-26 03:22:14 +0200

    Fix export in TrialBalancePage component

--- app/clients/[id]/trial-balance/page.tsx
+++ app/clients/[id]/trial-balance/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 
 // Redirect to the main client page since we've moved the trial balance there
-export default const TrialBalancePage = ({ params }: { params: { id: string } }) => {
+const TrialBalancePage = ({ params }: { params: { id: string } }) => {
   redirect(`/clients/${params.id}`);
-}
+};
+
+export default TrialBalancePage;
